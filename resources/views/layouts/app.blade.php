f32c642b424b956ba64a778fe6e159a619f4ca1c
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2023-08-13 09:55:49 +0600

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,9 +67,6 @@
         <p class="m-0 text-center text-white">Footer Page</p>
     </div>
 
-
-
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.pjax/2.0.1/jquery.pjax.min.js"></script>
     <script>
