f0178497420fe524ff17f0dc2ccb05930d0ebc90
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2023-10-29 11:49:29 +0600

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to the Home Page</h1>
+<h1>Welcome to the Home Page</h1>
+<a href="{{ url('database-backup') }}">Download Backup</a>
 @endsection
